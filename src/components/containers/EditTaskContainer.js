5f5150ecffff202726d2b1f88ef094970d55d808
Shuhei Yamaguchi <shuhei@SY-MacbookPro.lan>
2022-12-11 18:36:23 -0500

    added components

---
+++ src/components/containers/EditTaskContainer.js
@@ -0,0 +1,103 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+import { fetchTaskThunk, editTaskThunk } from '../../store/thunks';
+
+
+class EditTaskContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          title: "", 
+          due: "",
+          employeeId: null, 
+          redirect: false, 
+          redirectId: null
+        };
+    }
+
+    componentDidMount() {
+        //getting course ID from url
+        this.props.fetchTask(this.props.match.params.id);
+        this.setState({
+            title: this.props.course.title, 
+            due: this.props.course.due,
+            employeeId: this.props.course.employeeId, 
+        });
+      }
+
+    handleChange = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        //get new info for course from form input
+        let task = {
+            id: this.props.course.id,
+            title: this.state.title,
+            due: this.state.due,
+            employeeId: this.state.employeeId
+        };
+        
+        this.props.editTask(task);
+
+        this.setState({
+          redirect: true, 
+          redirectId: this.props.task.id
+        });
+
+    }
+
+    componentWillUnmount() {
+        this.setState({redirect: false, redirectId: null});
+    }
+
+    render() {
+      //go to single course view of the edited course
+        if(this.state.redirect) {
+          return (<Redirect to={`/course/${this.state.redirectId}`}/>)
+        }
+
+        return (
+            <form style={{textAlign: 'center'}} onSubmit={(e) => this.handleSubmit(e)}>
+            <label style= {{color:'#11153e', fontWeight: 'bold'}}>Title: </label>
+            <input type="text" name="title" value={this.state.title} onChange ={(e) => this.handleChange(e)}/>
+            <br/>
+
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>due: </label>
+            <input type="text" name="due" value={this.state.due} onChange={(e) => this.handleChange(e)}/>
+            <br/>
+  
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>instructorId: </label>
+            <input type="text" name="instructorId" value={this.state.instructorId} onChange={(e) => this.handleChange(e)} />
+            <br/>
+  
+            <button type="submit">
+              Submit
+            </button>
+
+          </form>
+        )
+    }
+}
+
+// map state to props
+const mapState = (state) => {
+    return {
+      task: state.task,
+    };
+  };
+
+const mapDispatch = (dispatch) => {
+    return({
+        editTask: (task) => dispatch(editTaskThunk(task)),
+        fetchTask: (id) => dispatch(fetchTaskThunk(id)),
+
+    })
+}
+
+export default connect(mapState, mapDispatch)(EditTaskContainer);
