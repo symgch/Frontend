931b08601811b10874fe0facad2546e88885e765
Shuhei Yamaguchi <shuhei@SY-MacbookPro.lan>
2022-12-11 17:00:46 -0500

    Update frontend

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ import store from "./store";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>,
     document.getElementById('root')
-);
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
